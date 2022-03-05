e204a2e9c0fbcb70c7522aa97634d54c4cf166aa
Kryštof <krystof.kratky2003@gmail.com>
2022-03-05 19:36:42 +0100

    react app

---
+++ react-app/src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://i.guim.co.uk/img/media/fe1e34da640c5c56ed16f76ce6f994fa9343d09d/0_174_3408_2046/master/3408.jpg?width=465&quality=45&auto=format&fit=max&dpr=2&s=cbe66879ae8c0ac09975d945f39137f6" className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,52 @@
 #include <Arduino.h>
 
-void setup() {
+#include <DNSServer.h>
+#ifdef ESP32
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#endif
+
+#include "LittleFS.h"
+#include "ESPAsyncWebServer.h"
+
+#include "config.h"
+
+AsyncWebServer server(80);
+
+
+void setup()
+{
   Serial.begin(115200);
+
+  if (!LittleFS.begin())
+  {
+    Serial.println("An Error has occurred while mounting LittleFS");
+    return;
+  }
+
+  File file = LittleFS.open("/build", "r");
+  if (!file)
+  {
+    Serial.println("Failed to open file for reading");
+    return;
+  };
+  WiFi.begin(ssid, password);
+
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.println(WiFi.localIP());
+  server.serveStatic("/", LittleFS, "/build");
+  server.begin();
 }
 
-void loop() {
-  Serial.println("Hello World!");
-  delay(1000);
+void loop()
+{
 }
